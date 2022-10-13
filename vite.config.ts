54719f52af37c38f1c963062eef2deb4ce4a5ef3
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-10-13 11:55:33 +0330

    feat: config the vite + first folders

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,85 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import * as path from "path";
+// @ts-ignore
+import { dependencies } from "./package.json";
+
+function renderChunks(deps: Record<string, string>) {
+	let chunks = {};
+	Object.keys(deps).forEach((key) => {
+		if (["react", "react-router-dom", "react-dom"].includes(key)) return;
+		chunks[key] = [key];
+	});
+	return chunks;
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "https://goat-dev.plan4better.de/api/v1",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
+	plugins: [
+		react(),
+		viteCompression({
+			ext: ".br",
+			algorithm: "brotliCompress",
+		}),
+	],
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ["react", "react-router-dom", "react-dom"],
+					...renderChunks(dependencies),
+				},
+			},
+		},
+		target: "es2021",
+		cssTarget: "chrome80",
+		chunkSizeWarningLimit: 500,
+	},
+	resolve: {
+		alias: [
+			{
+				find: "@types",
+				replacement: path.resolve(__dirname, "src/types"),
+			},
+			{
+				find: "@components",
+				replacement: path.resolve(__dirname, "src/components"),
+			},
+			{
+				find: "@utils",
+				replacement: path.resolve(__dirname, "src/utils"),
+			},
+			{
+				find: "@pages",
+				replacement: path.resolve(__dirname, "src/pages"),
+			},
+			{
+				find: "@styles",
+				replacement: path.resolve(__dirname, "src/styles"),
+			},
+			{
+				find: "@context",
+				replacement: path.resolve(__dirname, "src/context"),
+			},
+			{
+				find: "@api",
+				replacement: path.resolve(__dirname, "src/api"),
+			},
+			{
+				find: "@hooks",
+				replacement: path.resolve(__dirname, "src/hooks"),
+			},
+		],
+	},
 });
