1bf3ee93aa5cab4dfcf38fa2af42e7d3f41f1f5f
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-10-13 12:56:47 +0330

    feat: added sass to the project 🎨

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
 import router from "./Router";
+import "@styles/variables.scss";
+import "@styles/root.scss";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
