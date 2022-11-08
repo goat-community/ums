f10835ad308ec58cc7c0ddbd39292024362c68bf
Majk Shkurti <majkshkurti@outlook.com>
2022-11-08 14:37:26 +0100

    test: eslint husky

--- src/utils/log-error.ts
+++ src/utils/log-error.ts
@@ -1,7 +1,6 @@
 // Set a limit for logging errors so that we can log high usage functions that
 // would usually slow the application down from too much I/O
 export default function createLogError(maxLimit = 50, delayMs = 1) {
-  const test = "test";
   return function logError(...args) {
     if (maxLimit !== 0) {
       maxLimit -= 1;
