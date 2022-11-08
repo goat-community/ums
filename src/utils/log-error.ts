a6d197db066f1e4e1428bf40806ee35aa71df170
Majk Shkurti <majkshkurti@outlook.com>
2022-11-08 14:35:08 +0100

    test: quote change

--- src/utils/log-error.ts
+++ src/utils/log-error.ts
@@ -1,7 +1,8 @@
 // Set a limit for logging errors so that we can log high usage functions that
 // would usually slow the application down from too much I/O
 export default function createLogError(maxLimit = 50, delayMs = 1) {
-  return function logError(...args: any) {
+  const test = "test";
+  return function logError(...args) {
     if (maxLimit !== 0) {
       maxLimit -= 1;
       setTimeout(() => console.error(...args), delayMs);
