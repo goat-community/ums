004033eb559a04e290f73ec60a31102a722ce307
Sina <sina.farhadi.cyber@gmail.com>
2022-11-11 16:03:07 +0330

    refactor: flower page to handle more pages in future

--- src/pages/mobile/flower/Flower.tsx
+++ src/pages/mobile/flower/Flower.tsx
@@ -1,25 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Margin } from "@components/common";
-import { Box, Button, Typography } from "@mui/material";
-
-import "./flower.scss";
+import Introduction from "./components/introduction";
 
 export default function Flower() {
-  return (
-    <Box className="flower-form-container">
-      <Typography variant="h3">
-        The &apos;Proximity Flower&apos; helps map people&apos;s preferred city ammenities
-        in relation to the distance to their homes
-      </Typography>
-      <Margin margin="25px 0 0 0" />
-      <Typography variant="h3" fontWeight="bold">
-        How does you flower look like?
-      </Typography>
-      <Margin margin="35px 0 0 0" />
-      <Link to="/">
-        <Button variant="contained">Continue</Button>
-      </Link>
-    </Box>
-  );
+  const pages = [<Introduction key={"introduction"} />];
+  const pageIndex = 0;
+
+  return pages[pageIndex];
 }
