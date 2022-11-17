e3a316ffb32ba80b5c68a9a23599435b7dbfec04
Ebubeker Rexha <60944813+Ebubeker@users.noreply.github.com>
2022-11-17 12:05:05 +0100

    feat: implemented the translation files

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,1 +1,28 @@
-export {};
+import { initReactI18next } from "react-i18next";
+import axios from "axios";
+import i18next from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+function getAllTranslations() {
+  const languages = ["en", "de"];
+  const messages = {};
+  languages.forEach((language) => {
+    axios
+      .get(
+        `https://goat-community.github.io/translations/UMSTranslations/${
+          language || "en"
+        }/allTranslations.json`
+      )
+      .then((data) => {
+        messages[language] = { translation: data.data };
+      });
+  });
+  console.log(messages);
+  return messages;
+}
+
+i18next.use(initReactI18next).use(LanguageDetector).init({
+  debug: true,
+  fallbackLng: "en",
+  resources: getAllTranslations(),
+});
