f42f8e43b36a69b15977474a4a29139fa6fce139
Majk Shkurti <majkshkurti@outlook.com>
2022-11-23 13:41:58 +0100

    fix: types

--- src/pages/desktop/map/Map.tsx
+++ src/pages/desktop/map/Map.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import MapComponent from "@components/common/map";
 
 // import { SideBarLayout } from "@layouts/desktop";
-import { DrawerContent } from "./components/drawer-content";
-import { Header } from "./components/header";
+// import { DrawerContent } from "./components/drawer-content";
+// import { Header } from "./components/header";
 
 export default function Map() {
   return (
