da81187ab599dfdb9b9ca184fcb93fb0f5548386
Sina <sina.farhadi.cyber@gmail.com>
2022-11-25 14:31:24 +0330

    fix: flower modifier style

--- src/pages/mobile/map/Map.tsx
+++ src/pages/mobile/map/Map.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-// import { MapLayer } from "./components/map-layer";
-import { MapContainer } from "@containers/map/map-container";
 
+// import { MapLayer } from "./components/map-layer";
+// import { MapContainer } from "@containers/map/map-container";
 import { BottomBarLayout } from "@layouts/mobile";
 
 import { FloatingFlower } from "./components/floating-flower";
 import { MapHeader } from "./components/header";
+import { MapLayer } from "./components/map-layer";
 
 export default function Map() {
   return (
     <BottomBarLayout>
       <MapHeader />
       <FloatingFlower />
-      <MapContainer />
+      <MapLayer />
     </BottomBarLayout>
   );
 }
