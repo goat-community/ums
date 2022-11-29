e4f0087a5025f2594fffc04ffe7959ffa9536810
cinaaaa <sina.farhadi.cyber@gmail.com>
2022-11-29 16:45:03 +0330

    fix: remove question mark

--- src/pages/common/splash/index.tsx
+++ src/pages/common/splash/index.tsx
@@ -14,7 +14,7 @@ export default function Splash() {
       <div className="logo-container">
         <img src={logo} alt="logo" width="68" height="74" />
       </div>
-      <Typography variant="h1">Map4Citizens?</Typography>
+      <Typography variant="h1">Map4Citizens</Typography>
       <Margin margin="20px 0" />
       <CircularProgress />
     </section>
