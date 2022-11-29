3688a20318b3f6aa30d2ae2ceaa8cda9555ba8cf
cinaaaa <sina.farhadi.cyber@gmail.com>
2022-11-29 16:56:51 +0330

    fix: base url in production

--- src/api/isochrones.ts
+++ src/api/isochrones.ts
@@ -6,9 +6,7 @@ import { instance } from "@utils";
 export function getIsochrone(data: IsochroneParams): Promise<ArrayBuffer> | null {
   return instance
     .post("isochrones", data, { responseType: "arraybuffer" })
-    .then((response) => {
-      return response.data;
-    })
+    .then((response) => response.data)
     .catch((err: AxiosError) => {
       throw err;
     });
--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,4 +1,6 @@
 export function base_url(): string {
-  const base_url: string | undefined = "/api";
+  const base_url: string | undefined = import.meta.env.DEV
+    ? "/api"
+    : "https://goat-dev.plan4better.de/api/v1/";
   return base_url || "";
 }
