dca271a7fd24ab28f86ab6869a0ca325d227110b
cinaaaa <sina.farhadi.cyber@gmail.com>
2022-12-01 14:45:30 +0330

    feat: placeholder on insights page

--- src/pages/mobile/insights/Insights.tsx
+++ src/pages/mobile/insights/Insights.tsx
@@ -7,14 +7,27 @@ import { BottomBarLayout } from "@layouts/mobile";
 import { InsightsSection } from "./components/insights-section";
 import { ScoreList } from "./components/score-list";
 import { CenteredTabs } from "./components/tabs";
+import { Stack, Typography } from "@mui/material";
 
 export default function Insights() {
+  const has_isochrone_result = false;
   return (
     <BottomBarLayout>
       <Header />
-      <InsightsSection />
-      <ScoreList />
-      <CenteredTabs />
+      {has_isochrone_result ? (
+        <>
+          <InsightsSection />
+          <ScoreList />
+          <CenteredTabs />
+        </>
+      ) : (
+        <Stack padding={5}>
+          <Typography variant="h4" textAlign="center">
+            Please go back to the map section and pick an map point with clicking on the
+            flower button on the bottom right!
+          </Typography>
+        </Stack>
+      )}
     </BottomBarLayout>
   );
 }
