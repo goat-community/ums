1ea9bcb8dd74381eed1faa5572608dacd3c86460
cinaaaa <sina.farhadi.cyber@gmail.com>
2022-12-15 13:52:05 +0330

    feat: survey and map for desktop

--- src/components/mobile/header.tsx
+++ src/components/mobile/header.tsx
@@ -17,6 +17,7 @@ import * as D from "@constants/design";
 
 import Icon from "@images/icon.png";
 import Logo from "@images/m4c.png";
+
 export function Header() {
   const navigate = useNavigate();
   const survey_has_done = useAppSelector((state) => state.flower.survey_done_already);
---
+++ src/pages/desktop/flower/Flower.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+import Introduction from "./components/introduction";
+import Survey from "./components/survey";
+
+export default function Flower() {
+  const [pageIndex, setPageIndex] = useState<0 | 1>(0);
+  const pages = [
+    <Introduction key={"introduction"} onClickContinue={() => setPageIndex(1)} />,
+    <Survey key="survey" onClickBack={() => setPageIndex(0)} />,
+  ];
+
+  return pages[pageIndex];
+}
