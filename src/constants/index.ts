efde35b3b2ad14017d056d261ef7d73390b48d77
Sina Farhadi <sina.farhadi.cyber@gmail.com>
2022-12-16 13:33:53 +0330

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,7 +12,7 @@ export type PageKey = keyof typeof PAGES;
 export const MAPBOX_TOKEN =
   "pk.eyJ1IjoibWFqa3Noa3VydGkiLCJhIjoiY2w5YWpqM3ZlMGE2ZDQ1bGVqaG9tNzJnayJ9._IU92hy-v_e4vckpDNFyAQ";
 export const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE4NzUzODksInN1YiI6IjE3NyIsInNjb3BlcyI6W119.PMGeEuFD7AwqT2-Aab5LY25b4lYa4RK5o-n079N2fAs";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzE4NzYxNDMsInN1YiI6IjE0MiIsInNjb3BlcyI6W119.9HpBW7C4vFd3qzOM6ie4Vrg5y4AC07AZDxSnJomeajI";
 export const DEFAULT_ZOOM = 16;
 export const DEFAULT_LATITUDE = 48.1351;
 export const DEFAULT_LONGITUDE = 11.582;
