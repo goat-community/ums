b876322a84adc64653be347f6742e8537bd203d9
cinaaaa <sina.farhadi.cyber@gmail.com>
2022-12-30 16:27:08 +0330

    feat: mobile onboarding page

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,7 +12,7 @@ export type PageKey = keyof typeof PAGES;
 export const MAPBOX_TOKEN =
   "pk.eyJ1IjoibWFqa3Noa3VydGkiLCJhIjoiY2w5YWpqM3ZlMGE2ZDQ1bGVqaG9tNzJnayJ9._IU92hy-v_e4vckpDNFyAQ";
 export const API_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzI4Mjk2OTgsInN1YiI6IjE3NyIsInNjb3BlcyI6W119.EpvE12XneTjE0ifhBz5lgTNBlg4vxJHGES-xNoSUqrY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NzMwODQ4ODgsInN1YiI6IjE0MiIsInNjb3BlcyI6W119.51LKvUdFMeLhHazmk60Tj9T6v8g3m-sNnMhPyg5gfxc";
 export const DEFAULT_ZOOM = 16;
 export const DEFAULT_LATITUDE = 48.1351;
 export const DEFAULT_LONGITUDE = 11.582;
