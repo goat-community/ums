077b878613cea45d7ede1dc1254d37bd9a1dbb77
cinaaaa <sina.farhadi.cyber@gmail.com>
2023-06-06 15:13:21 +0330

    fix: harmonized the mobile version

--- src/pages/mobile/insights/components/pesonal-score.tsx
+++ src/pages/mobile/insights/components/pesonal-score.tsx
@@ -1,36 +1,62 @@
 import { useTranslation } from "react-i18next";
 import styled from "styled-components";
 
-import { CircularProgress, Stack, Typography } from "@mui/material";
+import { Box, CircularProgress, Stack, Typography } from "@mui/material";
 
 import { useCalculateSingleScore } from "@hooks";
 import { useAppSelector } from "@hooks/context";
 
-import { ScoreHighLighter } from "@components/common";
-
 export function PersonalScore() {
   const score = useCalculateSingleScore();
-  const isochrone_mode = useAppSelector((state) => state.isochrones.score_mode);
+  const address = useAppSelector((state) => state.map.current_point_address);
 
   const { t } = useTranslation();
 
   return (
     <Section>
-      <Stack direction="row" justifyContent="space-around" alignItems="center">
-        <CircularProgress
-          variant="determinate"
-          value={score * 10 || 0}
-          size="70px"
-          color="secondary"
-        />
-        <Stack justifyContent="space-between" alignItems="center">
-          <Typography variant="h6" fontSize="14px">
-            {t(`isochrone.modes.${isochrone_mode}`)} {t("insights.score")}
+      <Stack direction="row" justifyContent="space-between" alignItems="center">
+        <Stack justifyContent="space-between" alignItems="flex-start">
+          <Typography fontSize={15} fontWeight="bold" color="#283647">
+            {t("isochrone.modes.15min")}
           </Typography>
-          <Typography variant="h1" fontSize="46px" fontWeight="400">
-            <ScoreHighLighter isochrone_score={score} large />
+          <Typography
+            fontSize={13}
+            fontWeight="200"
+            color="#293748"
+            mt={1}
+            maxWidth={165}
+          >
+            {address?.display_name?.split(",").slice(0, 2) || ""}
+          </Typography>
+          <Typography variant="h6" fontWeight="200" color="#808894">
+            {address.address?.city}, {address.address?.state}, {address.address?.postcode}
+            , {address.address?.country}
           </Typography>
         </Stack>
+        <Box sx={{ position: "relative", display: "inline-flex" }}>
+          <CircularProgress
+            variant="determinate"
+            value={score * 10 || 0}
+            size="80px"
+            color="secondary"
+          />
+          <Box
+            sx={{
+              top: 0,
+              left: 0,
+              bottom: 0,
+              right: 0,
+              position: "absolute",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography variant="h5" component="div">
+              {score} / 10
+            </Typography>
+          </Box>
+        </Box>
       </Stack>
     </Section>
   );
@@ -38,7 +64,7 @@ export function PersonalScore() {
 
 const Section = styled.section`
   margin-top: 28px;
-  padding: 31px 23px;
+  padding: 11px 13px;
   box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.3), 0px 1px 3px 1px rgba(0, 0, 0, 0.15);
   border-radius: 10px;
   background: #fafafa;
